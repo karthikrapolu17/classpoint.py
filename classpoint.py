ac48118be25f3dc93e8f5f1570e6c895adac5812
Karthik Sai Rapolu <119866030+karthikrapolu17@users.noreply.github.com>
2022-12-27 20:45:56 +0530

    Create classpoint.py

---
+++ classpoint.py
@@ -0,0 +1,10 @@
+class Point():
+def __init__(self,n1,n2, n3):
+self.n1=n1
+self.n2=nz
+self.n3=n3
+def square(self):
+t=self.n1**2, self.n2**2, self.n3**2
+print (tuple(t))
+res-Point (n1=int (input ()),n2=int (input ()),n3=int (input()))
+res.square()
